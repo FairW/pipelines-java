aa80a52c140de3047edee918fcc9bce4f0d6077d
FairW <fair.winds.as@gmail.com>
2021-06-09 10:36:20 +0200

    Changed some println

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -3,10 +3,10 @@ package com.microsoft.demo;
 public class Demo {
     public void DoSomething(boolean flag){
         if(flag){
-            System.out.println("I am covered");
+            System.out.println("I am covered, Jarles edit 9th Jun 2021");
             return;
         }
 
-        System.out.println("I am not covered");
+        System.out.println("I am not covered, Jarles edit 9th Jun 2021");
     }
 }
